c2c6e4c350cbb5635410b7b697d1bb4a01cd8025
Magazzù Gaetano <lordfighterx@gmail.com>
2021-05-31 23:34:22 +0200

    Update dct.py

--- dct/dct.py
+++ dct/dct.py
@@ -21,13 +21,13 @@ def dct2n(A):
 
     C_temp1[0, :] = 1 * np.sqrt(1/N)
     for k in range(1, N):
-        for j in range(N):
-            C_temp1[k, j] = np.cos(np.pi * k * (2*j+1) / (2 * N )) * np.sqrt(2 / N)
+        for i in range(N):
+            C_temp1[k, i] = np.cos(np.pi * k * (2*i+1) / (2 * N )) * np.sqrt(2 / N)
 
     C_temp2[0, :] = 1 * np.sqrt(1/M)
     for q in range(1, M):
-        for i in range(M):
-            C_temp2[q, i] = np.cos(np.pi * q * (2*i+1) / (2 * M )) * np.sqrt(2 / M)
+        for j in range(M):
+            C_temp2[q, j] = np.cos(np.pi * q * (2*j+1) / (2 * M )) * np.sqrt(2 / M)
 
     out = np.dot(C_temp1 , A)
     out = np.dot(out, C_temp2)
