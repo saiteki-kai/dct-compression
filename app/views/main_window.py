bf51ef8e936fa2b034f47b57c9f7de075f58e2d0
Giuseppe <giuseppe.magazu@gmail.com>
2021-06-10 01:26:05 +0200

    add compress method

--- app/views/main_window.py
+++ app/views/main_window.py
@@ -3,8 +3,13 @@ import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
+import cv2
+from os import path
+
 from app.widgets.header_bar import HeaderBar
 from app.widgets.images_viewer import ImagesViewer
+
+from dct.compress import compress_image
 
 
 class MainWindow(Gtk.ApplicationWindow):
@@ -22,17 +27,42 @@ class MainWindow(Gtk.ApplicationWindow):
 
         self.add(box)
 
-    def on_image_changed(self, image):
-        self.image = image
-        self.viewer.set_original_image(image)
+    def on_image_changed(self, filename):
+        self.viewer.set_original_image(filename)
+        self.viewer.clear_compressed_image()
 
-    def on_compress_clicked(self, F, d):
-        if "image" not in self or not self.image:
+        # preload image for compression
+        self.img = cv2.imread(filename, 0)
+
+    def on_compress_clicked(self, filename, F, d):
+        if not filename:
             return
 
-        print(f"F: {F}, d: {d}, image: {self.image}")
+        if F < 0:
+            return
 
-        # do stuff with opencv
+        if d < 0 or d > 2 * F - 2:
+            messagedialog = Gtk.MessageDialog(
+                parent=self,
+                flags=Gtk.DialogFlags.MODAL,
+                type=Gtk.MessageType.WARNING,
+                buttons=Gtk.ButtonsType.OK_CANCEL,
+                message_format="Error",
+            )
+            # messagedialog.connect("response", self.dialog_response)
+            messagedialog.show()
+            return
 
-        # set image
-        # self.viewer.set_compressed_image(compressed)
+        print(f"F: {F}, d: {d}, image: {filename}")
+
+        self.viewer.clear_compressed_image()
+
+        img = self.img
+        if img is None:
+            img = cv2.imread(filename, 0)
+
+        out = compress_image(img, F, d)
+        out_filename = path.join("output", "part2", "out.bmp")
+        cv2.imwrite(out_filename, out)
+
+        self.viewer.set_compressed_image(out_filename)
---
+++ dct/compress.py
@@ -0,0 +1,70 @@
+from scipy import fftpack
+import numpy as np
+
+
+def compress_image(image, F, d):
+    # blocchettizzazione immagine
+    lista_blocchi = []
+    n_blocchi_a = (image.shape[0]) // F
+    n_blocchi_l = (image.shape[1]) // F
+    for x in range(n_blocchi_a):
+        for y in range(n_blocchi_l):
+            lista_blocchi.append(image[x * F : ((x + 1) * F), y * F : ((y + 1) * F)])
+
+    # applico la dct2 ai blocchi
+    lista_dct2 = []
+    for x in lista_blocchi:
+        lista_dct2.append(fftpack.dctn(x, norm="ortho"))
+
+    # eliminazione frequenze
+    for x in lista_dct2:
+        for k in range(F):
+            for l in range(F):
+                if k + l >= d:
+                    x[k, l] = 0
+
+    # applico la IDCT2 ai blocchi modificati
+    lista_idct2 = []
+    for x in lista_dct2:
+        lista_idct2.append(fftpack.idctn(x, norm="ortho"))
+
+    # arrotondamento valori
+    lista_round = []
+    for x in lista_idct2:
+        x = x.clip(0, 255)
+        lista_round.append(x.round())
+
+    # ricomposizione matrice immagine
+    mat_ricostruzione = []
+    for y in range(n_blocchi_a):
+        mat_ricostruzione.append(
+            np.concatenate(
+                (lista_round[y * n_blocchi_l : (y + 1) * n_blocchi_l]), axis=1
+            )
+        )
+
+    out = np.concatenate((mat_ricostruzione), axis=0)
+
+    # N, M = img.shape
+
+    # n_blocks = N // F
+    # m_blocks = M // F
+
+    # out = np.zeros((n_blocks * F, m_blocks * F), dtype=np.uint8)
+
+    # for i in range(n_blocks):
+    #     for j in range(m_blocks):
+    #         f = img[i * F : i * F + F, j * F : j * F + F]
+    #         c = dctn(f, norm="ortho")
+
+    #         for k in range(F):
+    #             for l in range(F):
+    #                 if k + l >= d:
+    #                     c[k, l] = 0
+
+    #         ff = idctn(c, norm="ortho")
+    #         ff = ff.round().clip(0, 255)
+
+    #         out[i * F : i * F + F, j * F : j * F + F] = ff
+
+    return out
